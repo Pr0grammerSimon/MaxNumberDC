17a10f09e6153efac1618255bb65ce088dbf4074
Pr0grammerSimon <szymon@krzywda.eu>
2025-07-14 12:12:35 +0200

    Still fixing bugs

--- game.py
+++ game.py
@@ -9,7 +9,6 @@ NR_NUMBER_CARDS = 10
 
 log = logging.getLogger("game")
 
-
 class Game:
 
     def __init__(self, player1_id: int, player2_id: int,
@@ -33,7 +32,7 @@ class Game:
         self.channel2 = channel2
         self.messages = []
 
-        # stan wyborów
+        # selection state (reset each turn)
         self.player_choice = None
         self.card_choice = None
         self.pos_choice = None
@@ -44,14 +43,25 @@ class Game:
         try:
             g1 = self.channel1.guild
             g2 = self.channel2.guild
-            self.player1 = g1.get_member(self.player1_id) or await g1.fetch_member(self.player1_id)
-            self.player2 = g2.get_member(self.player2_id) or await g2.fetch_member(self.player2_id)
-            if not (self.player1 and self.player2):
-                log.warning("Nie znaleziono któregoś z graczy")
+
+            # Fetch first player without raising on NotFound
+            try:
+                self.player1 = g1.get_member(self.player1_id) or await g1.fetch_member(self.player1_id)
+            except discord.errors.NotFound:
+                self.player1 = None
+
+            # Fetch second player
+            try:
+                self.player2 = g2.get_member(self.player2_id) or await g2.fetch_member(self.player2_id)
+            except discord.errors.NotFound:
+                self.player2 = None
+
+            if not self.player1 or not self.player2:
+                log.warning(f"Could not find one of the players: p1={self.player1_id}->{self.player1}, p2={self.player2_id}->{self.player2}")
                 return False
             return True
-        except Exception as e:
-            log.error("Błąd w setup_players", exc_info=True)
+        except Exception:
+            log.error("Error in setup_players", exc_info=True)
             return False
 
     async def start(self):
@@ -63,16 +73,9 @@ class Game:
             return
 
         embed = await self.cards_embed()
-        if self.channel1 == self.channel2:
-            view = PlayerChoiceView(self)
-            self.messages = [await self.channel1.send(embed=embed, view=view)]
-        else:
-            view1 = PlayerChoiceView(self)
-            view2 = PlayerChoiceView(self)
-            self.messages = [
-                await self.channel1.send(embed=embed, view=view1),
-                await self.channel2.send(embed=embed, view=view2)
-            ]
+        view = PlayerChoiceView(self)
+        target = self.channel1 if self.current_turn == self.player1_id else self.channel2
+        self.messages = [await target.send(embed=embed, view=view)]
 
     async def cards_embed(self) -> discord.Embed:
         e = discord.Embed(
@@ -82,17 +85,17 @@ class Game:
         )
         e.add_field(
             name=f"{self.player1.display_name} cards",
-            value=f"```{''.join(map(str, self.cards[self.player1_id]))}```",
+            value=f"```{' '.join(map(str, self.cards[self.player1_id]))}```",
             inline=False
         )
         e.add_field(
             name="Available",
-            value=f"```{', '.join(map(str, self.available))}```",
+            value=f"```{' '.join(map(str, self.available))}```",
             inline=False
         )
         e.add_field(
             name=f"{self.player2.display_name} cards",
-            value=f"```{''.join(map(str, self.cards[self.player2_id]))}```",
+            value=f"```{' '.join(map(str, self.cards[self.player2_id]))}```",
             inline=False
         )
         e.add_field(
@@ -105,31 +108,32 @@ class Game:
         return e
 
     def can_press(self, user_id: int) -> bool:
-        """Tylko sprawdza, czy user_id może wykonać ruch."""
-        if user_id not in (self.player1_id, self.player2_id):
-            return False
-        if user_id != self.current_turn:
-            return False
-        return True
+        return user_id in (self.player1_id, self.player2_id) and user_id == self.current_turn
 
     async def valid_move(self) -> bool:
         try:
             expr = ''.join(map(str, self.cards[self.player_choice]))
             expr = expr[:self.pos_choice] + str(self.card_choice) + expr[self.pos_choice:]
-            # dwa operatory obok
             for a, b in zip(expr, expr[1:]):
                 if a in OPERATION_CARDS and b in OPERATION_CARDS:
                     return False
-            # nie zaczyna się od */ i nie kończy operatorem
             if expr[0] in ('*', '/') or expr[-1] in OPERATION_CARDS:
+                return False
+            if expr.startswith('--'):
                 return False
             return True
         except Exception:
             return False
 
     async def end_embed(self) -> discord.Embed:
-        s1 = eval(''.join(map(str, self.cards[self.player1_id])))
-        s2 = eval(''.join(map(str, self.cards[self.player2_id])))
+        try:
+            s1 = eval(''.join(map(str, self.cards[self.player1_id])))
+        except ZeroDivisionError:
+            s1 = float('-inf')
+        try:
+            s2 = eval(''.join(map(str, self.cards[self.player2_id])))
+        except ZeroDivisionError:
+            s2 = float('-inf')
         if s1 > s2:
             desc = f"{self.player1.display_name} WON!"
         elif s2 > s1:
@@ -151,18 +155,14 @@ class Game:
         )
         return e
 
-
 # ---- UI ----
 
 class SafeInteraction:
-    """Pomocnicze metody na interakcję."""
     @staticmethod
     async def defer(interaction: discord.Interaction):
         try:
             await interaction.response.defer()
-        except discord.errors.InteractionResponded:
-            pass
-        except discord.errors.NotFound:
+        except (discord.errors.InteractionResponded, discord.errors.NotFound):
             pass
 
     @staticmethod
@@ -172,9 +172,10 @@ class SafeInteraction:
                 await interaction.response.send_message(**kwargs)
             else:
                 await interaction.followup.send(**kwargs)
-        except Exception as e:
-            log.error(f"Failed to respond: {e}", exc_info=True)
-
+        except (discord.errors.NotFound, discord.errors.InteractionResponded):
+            pass
+        except Exception:
+            log.error("Failed to respond", exc_info=True)
 
 class BackButton(discord.ui.Button):
     def __init__(self, game: Game):
@@ -184,20 +185,16 @@ class BackButton(discord.ui.Button):
     async def callback(self, interaction: discord.Interaction):
         user = interaction.user.id
         if not self.game.can_press(user):
-            await SafeInteraction.respond(interaction, content="Nie Twoja kolej!", ephemeral=True)
-            return
+            return await SafeInteraction.respond(interaction, content="It's not your turn!", ephemeral=True)
         await SafeInteraction.defer(interaction)
 
-        # cofnij stan
         if self.game.choice_nr > 0:
             self.game.choice_nr -= 1
 
-        # edytuj widok
         view = PlayerChoiceView(self.game) if self.game.choice_nr == 0 else CardChoiceView(self.game)
         embed = await self.game.cards_embed()
         await interaction.message.edit(embed=embed, view=view)
-        await SafeInteraction.respond(interaction, content="Cofnięto", ephemeral=True)
-
+        await SafeInteraction.respond(interaction, content="Reverted", ephemeral=True)
 
 class PlayerChoiceView(discord.ui.View):
     def __init__(self, game: Game):
@@ -206,7 +203,6 @@ class PlayerChoiceView(discord.ui.View):
         game.choice_nr = 0
         self.add_item(PlayerChoiceButton(game.player1, game))
         self.add_item(PlayerChoiceButton(game.player2, game))
-
 
 class PlayerChoiceButton(discord.ui.Button):
     def __init__(self, player: discord.Member, game: Game):
@@ -217,15 +213,13 @@ class PlayerChoiceButton(discord.ui.Button):
     async def callback(self, interaction: discord.Interaction):
         user = interaction.user.id
         if not self.game.can_press(user):
-            await SafeInteraction.respond(interaction, content="Nie Twoja kolej!", ephemeral=True)
-            return
+            return await SafeInteraction.respond(interaction, content="It's not your turn!", ephemeral=True)
 
         await SafeInteraction.defer(interaction)
         self.game.player_choice = self.player.id
         self.game.choice_nr = 1
         embed = await self.game.cards_embed()
         await interaction.message.edit(embed=embed, view=CardChoiceView(self.game))
-
 
 class CardChoiceView(discord.ui.View):
     def __init__(self, game: Game):
@@ -236,7 +230,6 @@ class CardChoiceView(discord.ui.View):
             self.add_item(CardChoiceButton(c, game))
         self.add_item(BackButton(game))
 
-
 class CardChoiceButton(discord.ui.Button):
     def __init__(self, card, game: Game):
         super().__init__(label=str(card), style=discord.ButtonStyle.secondary)
@@ -246,25 +239,26 @@ class CardChoiceButton(discord.ui.Button):
     async def callback(self, interaction: discord.Interaction):
         user = interaction.user.id
         if not self.game.can_press(user):
-            await SafeInteraction.respond(interaction, content="Nie Twoja kolej!", ephemeral=True)
-            return
+            return await SafeInteraction.respond(interaction, content="It's not your turn!", ephemeral=True)
 
         await SafeInteraction.defer(interaction)
         self.game.card_choice = self.card
         self.game.choice_nr = 2
         embed = await self.game.cards_embed()
+        if self.game.player_choice is None:
+            return await SafeInteraction.respond(interaction, content="Please choose a player first!", ephemeral=True)
         await interaction.message.edit(embed=embed, view=PositionChoiceView(self.game))
-
 
 class PositionChoiceView(discord.ui.View):
     def __init__(self, game: Game):
         super().__init__(timeout=None)
         self.game = game
         game.choice_nr = 2
+        if game.player_choice is None:
+            raise RuntimeError("player_choice is None")
         for i in range(len(game.cards[game.player_choice]) + 1):
             self.add_item(PositionChoiceButton(i, game))
         self.add_item(BackButton(game))
-
 
 class PositionChoiceButton(discord.ui.Button):
     def __init__(self, pos: int, game: Game):
@@ -275,29 +269,23 @@ class PositionChoiceButton(discord.ui.Button):
     async def callback(self, interaction: discord.Interaction):
         user = interaction.user.id
         if not self.game.can_press(user):
-            await SafeInteraction.respond(interaction, content="Nie Twoja kolej!", ephemeral=True)
-            return
+            return await SafeInteraction.respond(interaction, content="It's not your turn!", ephemeral=True)
 
         await SafeInteraction.defer(interaction)
         self.game.pos_choice = self.pos
         if not await self.game.valid_move():
-            await SafeInteraction.respond(interaction, content="Invalid move!", ephemeral=True)
-            return
+            return await SafeInteraction.respond(interaction, content="Invalid move!", ephemeral=True)
 
-        # wykonaj ruch
-        self.game.cards[user].insert(self.pos, self.game.card_choice)
+        self.game.cards[self.game.player_choice].insert(self.pos, self.game.card_choice)
         self.game.available.remove(self.game.card_choice)
-        # reset
         self.game.card_choice = None
         self.game.player_choice = None
         self.game.pos_choice = None
-        # zmiana kolejki
         self.game.current_turn = (
             self.game.player2_id
             if user == self.game.player1_id
             else self.game.player1_id
         )
-        # zaktualizuj widok / embed
         if not self.game.available:
             embed = await self.game.end_embed()
             await interaction.message.edit(embed=embed, view=None)
